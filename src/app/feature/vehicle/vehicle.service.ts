5c53dc4405dfa27f5a67b4760dc8963aff753407
Greg Doud <gpdoud@gmail.com>
2018-05-08 11:00:54 -0400

    1) Changed baseurl in index.html to ./ ONLY for productoin build 2) Change Deerfield Township logo to a normal image in the heading component 3) Changed the vehicle.server to use the dynamic url to the back-end service

--- src/app/feature/vehicle/vehicle.service.ts
+++ src/app/feature/vehicle/vehicle.service.ts
@@ -1,33 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-
+import { SystemService } from '@feat/system/system.service';
 import { Vehicle } from '@feat/vehicle/vehicle';
-
-const url = "http://localhost:44444/api/Vehicles/";
 
 @Injectable()
 export class VehicleService {
 
-  login(vehiclename: string, password: string): Observable<Vehicle> {
-    return this.http.get(url+`Login?vehiclename=${vehiclename}&password=${password}`) as Observable<Vehicle>;
-  }
+  url = this.syssvc.settings.baseurl + "api/Vehicles/";
+
   list(): Observable<Vehicle[]> {
-    return this.http.get(url+"List") as Observable<Vehicle[]>;
+    return this.http.get(this.url+"List") as Observable<Vehicle[]>;
   }
   get(id: number): Observable<Vehicle> {
-    return this.http.get(url+"Get/"+id) as Observable<Vehicle>;
+    return this.http.get(this.url+"Get/"+id) as Observable<Vehicle>;
   }
   create(vehicle: Vehicle): Observable<boolean> {
-    return this.http.post(url+"Create", vehicle) as Observable<boolean>;
+    return this.http.post(this.url+"Create", vehicle) as Observable<boolean>;
   }
   change(vehicle: Vehicle): Observable<boolean> {
-    return this.http.post(url+"Change", vehicle) as Observable<boolean>;
+    return this.http.post(this.url+"Change", vehicle) as Observable<boolean>;
   }
   remove(vehicle: Vehicle): Observable<boolean> {
-    return this.http.post(url+"Remove", vehicle) as Observable<boolean>;
+    return this.http.post(this.url+"Remove", vehicle) as Observable<boolean>;
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private syssvc: SystemService) { }
 
 }
