12d4d281e6981afc68d63e9e7aca0e4a4cf7fbd7
Elisha Amos <elisha.amos316@yahoo.com>
2018-12-20 19:01:36 -0500

    Adding Buildcost total and Personal property cost on department level.

--- src/app/feature/department/department.ts
+++ src/app/feature/department/department.ts
@@ -3,11 +3,15 @@ export class Department {
   Id: number;
   Code: string;
   Name: string;
+  BuildingCost?: number;
+  PersonalPropertyCost?: number;
   Active: boolean;
 
-  constructor(Code: string, Name: string, Active: boolean) {
+  constructor(Code: string, Name: string, Active: boolean, BuildingCost?: number, PersonalPropertyCost?: number) {
     this.Code = Code;
     this.Name = Name;
+    this.BuildingCost = BuildingCost;
+    this.PersonalPropertyCost = PersonalPropertyCost;
     this.Active = Active;
   }
 
